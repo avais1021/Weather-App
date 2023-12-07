3d877273e213f8d1dbfe97e49b8defcc0b19ba62
Owes Ansari <Owes.Ansari@teknopoint.local>
2023-12-07 12:53:22 +0530

    change api key

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API_KEY = `3265874a2c77ae4a04bb96236a642d2f`;
+const API_KEY = `0d42adec81f5602776bfde00b8ad6849`;
 // const API = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
 // const IMG_URL = `https: //openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
 
